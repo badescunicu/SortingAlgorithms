f088de2cd2477b4c933ee7652b21a022b698a93a
Nicu Badescu <badescunicu@yahoo.com>
2013-11-14 16:00:49 +0200

    Implement Shellsort

--- sortAlgo.c
+++ sortAlgo.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define GAPS_SIZE 8
+
 /* Swap 2 values */
 void swap(int *a, int *b) {
     int aux;
@@ -44,13 +46,32 @@ void insertion_sort(int *to_sort, int size) {
     }
 }
 
+/* Shellsort using Ciura's gaps */
+void shellsort(int *to_sort, int size) {
+    int gaps[] = {701, 301, 132, 57, 23, 10, 4, 1};
+    int gap_iterator, i, j;
+    /* Cycle through gaps */
+    for (gap_iterator = 0; gap_iterator < GAPS_SIZE; gap_iterator++) {
+        /* Perform an Insertion Sort for each gap */
+        for (i = gaps[gap_iterator]; i < size; i++) {
+            for (j = i; j >= gaps[gap_iterator]; j--) {
+                if (to_sort[j] < to_sort[j - gaps[gap_iterator]]) {
+                    swap(&to_sort[j], &to_sort[j - gaps[gap_iterator]]);
+                }
+            }
+        }
+    }
+}
+
 int main() {
     int *v = malloc(10 * sizeof(int));
     int i, j;
     for (i = 0, j = 10; i < 10; i++)
         v[i] = j--;
+    v[6] = -4;
+    v[8] = -1;
     print(v, 10);
-    insertion_sort(v, 10);
+    shellsort(v, 10);
     print(v, 10);
 
     return 0;
